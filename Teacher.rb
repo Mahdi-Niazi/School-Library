0322bf50debdfcecfeca7945fbdfee0900fcbc8d
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-10-02 12:56:11 +0430

    Fix linter errors

--- Teacher.rb
+++ Teacher.rb
@@ -1,13 +1,12 @@
 require_relative 'Person'
 
-class Teacher
-    def initialize(name = "Unknown", age, parent_permission=true, specialization)
-        super(name, age, parent_permission)
-        @specialization = specialization
-    end
-    def can_use_services?
-        true
-    end
-end
+class Teacher < Person
+  def initialize(age, specialization, name = 'Unknown')
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
 
-    
+  def can_use_services?
+    true
+  end
+end
