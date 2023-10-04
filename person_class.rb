9fdee586243b8e52d8962e2c2a46f24d70eee12f
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-10-04 12:43:13 +0430

    Add association between classes

--- person_class.rb
+++ person_class.rb
@@ -2,7 +2,7 @@ require './nameable'
 require './decorators'
 
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -11,6 +11,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def of_age?
@@ -25,11 +26,8 @@ class Person < Nameable
   def correct_name
     @name
   end
-end
 
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+  end
+end
